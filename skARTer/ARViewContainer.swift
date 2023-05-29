82417b80b522f53b4685d957292aea34fca36ad0
B8B_csabi <bladeszasza@gmail.com>
2023-05-29 17:43:16 +0200

    added long press

--- skARTer/ARViewContainer.swift
+++ skARTer/ARViewContainer.swift
@@ -15,13 +15,13 @@ struct ARViewContainer: UIViewRepresentable {
     var arView: ARView = ARView(frame: .zero)
     @Binding var skateboardEntity: Entity? // Used to store the skateboard entity
      @State private var impulseStartDate: Date? = nil // Used to store the start date of impulse
-     @State private var userDirection: SIMD3<Float> = SIMD3<Float>(0, 0, 0) // Direction of the user
+     @State private var userDirection: SIMD3<Float> = SIMD3<Float>(0, -1, 0) // Direction of the user
     
     let impulseDuration: TimeInterval = 1 // in seconds
 
     
     func makeUIView(context: Context) -> ARView {
-        arView.debugOptions = .showPhysics
+//        arView.debugOptions = .showPhysics
         
         // Set up the ARView's session configuration for LiDAR scanning
         let config = ARWorldTrackingConfiguration()
@@ -57,6 +57,10 @@ struct ARViewContainer: UIViewRepresentable {
         let tapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap(_:)))
         arView.addGestureRecognizer(tapGesture)
         
+        // Add tap gesture recognizer
+        let longPressGesture = UILongPressGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleLongPress(_:)))
+        arView.addGestureRecognizer(longPressGesture)
+        
         return arView
     }
     
@@ -81,24 +85,84 @@ struct ARViewContainer: UIViewRepresentable {
             self.parent = parent
         }
         
-        @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        @objc func handleLongPress(_ sender: UILongPressGestureRecognizer){
             // Perform hit test
             let location = sender.location(in: parent.arView)
             let results = parent.arView.raycast(from: location, allowing: .estimatedPlane, alignment: .any)
+            
+            switch sender.state {
+            case .began:
             
             if let firstResult = results.first {
                 // Apply impulse on the tapped location
                 if let skateboardWithPhysics = parent.skateboardEntity as? HasPhysics {
                     let position = SIMD3<Float>(firstResult.worldTransform.columns.3.x, firstResult.worldTransform.columns.3.y, firstResult.worldTransform.columns.3.z)
-                    skateboardWithPhysics.applyImpulse(parent.userDirection * pushStrength, at: position, relativeTo: nil)
+                    
+                    print("position: \(position)")
+                    print("strength: \(parent.userDirection * kickStrength)")
+                    
+                    skateboardWithPhysics.physicsBody?.massProperties.centerOfMass.position = position
+                    skateboardWithPhysics.addForce(parent.userDirection * kickStrength, at: position, relativeTo: nil)
+                    
+
                 }
             }
+            case .ended:
+                // User has ended a tap, reset the center of mass
+                if let skateboardWithPhysics = parent.skateboardEntity as? HasPhysics {
+                    skateboardWithPhysics.physicsBody?.massProperties.centerOfMass.position = [0.0, 0.0, 0.0]
+                }
+                
+            default:
+                break
+            }
+                
+        }
+        
+        
+
+        
+        
+        @objc func handleTap(_ sender: UITapGestureRecognizer) {
+            // Perform hit test
+            let location = sender.location(in: parent.arView)
+            let results = parent.arView.raycast(from: location, allowing: .estimatedPlane, alignment: .any)
+            
+            switch sender.state {
+            case .began:
+                // User has started a tap, apply the impulse
+                if let firstResult = results.first {
+                    // Apply impulse on the tapped location
+                    if let skateboardWithPhysics = parent.skateboardEntity as? HasPhysics {
+                        let position = SIMD3<Float>(firstResult.worldTransform.columns.3.x, firstResult.worldTransform.columns.3.y, firstResult.worldTransform.columns.3.z)
+                        
+                        print("position: \(position)")
+                        print("strength: \(parent.userDirection * kickStrength)")
+    
+                        skateboardWithPhysics.physicsBody?.massProperties.centerOfMass.position = position
+                        skateboardWithPhysics.applyImpulse(parent.userDirection * kickStrength, at: position, relativeTo: nil)
+                    }
+                }
+                
+            case .ended:
+                // User has ended a tap, reset the center of mass
+                if let skateboardWithPhysics = parent.skateboardEntity as? HasPhysics {
+                    skateboardWithPhysics.physicsBody?.massProperties.centerOfMass.position = [0.0, 0.0, 0.0]
+                }
+                
+            default:
+                break
+            }
+            
+            
         }
     }
 }
 
-var pushStrength: Float = 0.1 // adjust this to achieve the desired push strength
+var pushStrength: Float = 5.8 // adjust this to achieve the desired push strength
+var kickStrength: Float = 0.8 // adjust this to achieve the desired kick strength
 let tailCoordinates  = SIMD3<Float>(0.385,-0.0497,0.0)
+let noseCoordinates  = SIMD3<Float>(-0.385,-0.0497,0.0)
 
 
 //deszka : [hossza, magassaga, szelessege]
@@ -113,22 +177,15 @@ func applyPhysicsAndCollision(to entity: HasPhysics) {
     ]
     entity.collision = CollisionComponent(shapes: shapes)
     entity.physicsBody = PhysicsBodyComponent(shapes: shapes, mass: 4.5)
-    entity.physicsBody?.massProperties.centerOfMass.position = [0.305, 0.0, 0.0]
 }
 
 extension ARViewContainer {
 func startImpulse() {
-        impulseStartDate = Date()
-        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
-            guard let impulseStartDate = impulseStartDate else { return }
-            if Date().timeIntervalSince(impulseStartDate) >= impulseDuration {
-                timer.invalidate()
-            } else {
-                if let skateboardWithPhysics = skateboardEntity as? HasPhysics {
-                    skateboardWithPhysics.applyImpulse(userDirection * pushStrength, at: tailCoordinates, relativeTo: nil)
-                }
-            }
-        }
+    
+    if let skateboardWithPhysics = skateboardEntity as? HasPhysics {
+        skateboardWithPhysics.physicsBody?.massProperties.centerOfMass.position = [0.0, 0.0, 0.0]
+        skateboardWithPhysics.physicsMotion?.linearVelocity += (userDirection * pushStrength)
+    }
     }
 }
 
